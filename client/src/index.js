f730a2e3eac7147557c8752eaf245ef23670ef94
Andrei Yasinski <andrewtopol856@gmail.com>
2020-03-27 12:51:04 +0300

    update redux dev tools implementation

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { createStore } from 'redux';
 import chatApp from './reducers';
 import App from './components/app/app';
 
-const store = createStore(chatApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(chatApp, window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f)
 
 ReactDOM.render(
   <Provider store={store}>
